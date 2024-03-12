5a250517abee0bbf3d498471d684eaad7d01dd04
Yuxiang Feng <2162381070@qq.com>
2024-03-12 14:34:25 +1100

    Initial Commit

---
+++ Insomnia-Solution-App-FIT3162/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insomnia-Solution-App-FIT3162
+//
+//  Created by Yuxiang Feng on 12/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

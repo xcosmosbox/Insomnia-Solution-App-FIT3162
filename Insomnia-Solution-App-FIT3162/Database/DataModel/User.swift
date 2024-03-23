b644bef52c86221d70d3c4ac8b8fc61dff3ec5e0
San_Check <2162381070@qq.com>
2024-03-23 22:47:35 +1100

    Create User.swift

---
+++ Insomnia-Solution-App-FIT3162/Database/DataModel/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Insomnia-Solution-App-FIT3162
+//
+//  Created by Yuxiang Feng on 23/3/2024.
+//
+
+import Foundation
+
+struct User {
+    var email: String
+    var name: String
+    var userCover: String
+}
+

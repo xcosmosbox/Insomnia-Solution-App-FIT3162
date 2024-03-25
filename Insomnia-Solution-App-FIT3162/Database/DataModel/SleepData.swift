a01cb887760bb52fb1e203604f540e114c865abe
San_Check <2162381070@qq.com>
2024-03-25 16:14:47 +1100

    create SleepData structure

---
+++ Insomnia-Solution-App-FIT3162/Database/DataModel/SleepData.swift
@@ -0,0 +1,14 @@
+//
+//  SleepData.swift
+//  Insomnia-Solution-App-FIT3162
+//
+//  Created by Yuxiang Feng on 25/3/2024.
+//
+
+import Foundation
+
+struct SleepData {
+    var date: Date
+    var startTime: Date
+    var endTime: Date
+}

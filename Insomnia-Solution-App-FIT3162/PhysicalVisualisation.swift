1b4c69b1042daec99da3fc5cccb73efba1a1ec23
snil0002 <snil0002@student.monash.edu>
2024-03-26 12:18:27 +1100

    Add page for Visualisation

---
+++ Insomnia-Solution-App-FIT3162/PhysicalVisualisation.swift
@@ -0,0 +1,18 @@
+//
+//  PhysicalVisualisation.swift
+//  Insomnia-Solution-App-FIT3162
+//
+//  Created by Niloy Shehrin on 26/3/2024.
+//
+
+import SwiftUI
+
+struct PhysicalVisualisation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhysicalVisualisation()
+}
